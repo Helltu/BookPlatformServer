232f5968c74d6f377917c3a6ecafbed58a96369f
Hellty <korsovm@gmail.com>
2024-10-08 18:07:19 +0300

    initial commit

---
+++ src/test/java/by/bsuir/bookplatform/BookPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.bookplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
