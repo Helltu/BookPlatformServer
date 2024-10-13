67cb79136a8779bba023f6f44b25e76435653ee3
Hellty <korsovm@gmail.com>
2024-10-13 13:57:47 +0300

    Add more constructors to CartId

--- src/main/java/by/bsuir/bookplatform/entities/CartId.java
+++ src/main/java/by/bsuir/bookplatform/entities/CartId.java
@@ -1,12 +1,17 @@
 package by.bsuir.bookplatform.entities;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.io.Serializable;
 
 @Embeddable
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CartId implements Serializable {
 
     private Long userId;
