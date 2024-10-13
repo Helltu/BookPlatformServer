80120cd8ef3bc4cef3284a4294b44563e891b0f6
Hellty <korsovm@gmail.com>
2024-10-13 14:55:55 +0300

    Add more constructors to ReviewId

--- src/main/java/by/bsuir/bookplatform/entities/ReviewId.java
+++ src/main/java/by/bsuir/bookplatform/entities/ReviewId.java
@@ -1,12 +1,16 @@
 package by.bsuir.bookplatform.entities;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Embeddable
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewId implements Serializable {
 
     private Long bookId;
